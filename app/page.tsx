807ca01b789b4a150a54e0baa8421e9025df4202
JoaoVictorCRP <joaovictorxl120@gmail.com>
2025-04-12 20:43:06 -0300

    feat | operacao de raiz quadrada

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,34 @@
 import Visor from "@/app/components/Visor";
 import {Numpad} from "@/app/components/Numpad";
 import {useState} from "react";
+import Swal from "sweetalert2";
 
 export default function Home() {
     const [input, setInput] = useState('0');
 
     const handleInput = (val: string) => {
         if (val=='=') {
-            setInput(eval(input));
+            // Substitui todas as ocorrências de "√" seguido de número ou expressão (ex: √9, √(9+1), etc.)
+            const processedInput = input.replace(/√(\d+(\.\d+)?|\([^)]+\))/g, (_, expr) => {
+                return `Math.sqrt(${expr})`;
+            });
+
+            const result= eval(processedInput);
+
+            if (result===Infinity) { // previnir divisão por zero com o sweetalert
+                Swal.fire({
+                    title: "Ops!",
+                    text: "Não é possível dividir por zero.",
+                    icon: "question"
+                }).then(res => {
+                    if (res.isConfirmed) {
+                        setInput('0');
+                    }
+                });
+                return;
+            }
+
+            setInput(result);
             return;
         }
 
@@ -17,11 +38,34 @@ export default function Home() {
             return;
         }
 
+        if (val=='÷'){
+            setInput((prev) => prev + '/');
+            return;
+        }
+
+        if (val=='÷'){
+            setInput((prev) => prev + '/');
+            return;
+        }
+
         if (val==',') {
             setInput((prev) => prev + '.');
             return;
         }
 
+        if (val==='( )'){
+            if (input.includes('(')) {
+                setInput((prev) => prev + ')');
+                return;
+            }
+
+            if (input==='0') {
+                setInput('(');
+                return;
+            }
+            setInput((prev) => prev + '(');
+            return;
+        }
 
         if (val=='C'){
             setInput('0');
